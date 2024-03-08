caf3d97f76e422277d56782cddf38bd61a6580eb
@jpvalery <jpvalery@users.noreply.github.com>
2024-03-08 11:50:12 -0500

    feat: add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
 	content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				verdant: '#E4FFCE',
+				evergreen: '#0B353B',
+				cosmic: '#FFF5C1',
+				mauve: '#FCECFF',
+				lavender: '#D6D8FF',
+				arctic: '#C0F8F8',
+				warmGray: '#FBF9F8',
+				coconut: '#27201D',
+				sangria: '#3B0B22',
+				indigo: '#2C155D',
+				moss: '#2E340C',
+				charcoal: '#151515',
+				tertiaryTeal: '#44D4DE',
+				tertiaryBlue: '#0075FF',
+				tertiaryPurple: '#533FCD',
+				tertiaryYellow: '#F7B83F',
+				tertiaryOrange: '#FD7742',
+				tertiaryPlum: '#95DE5F',
+			},
+		},
 	},
 	plugins: [],
 };
